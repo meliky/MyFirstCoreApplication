6095f50f8d1b58972a08467e3ae8a5047cc5f414
meliky <melikyucel@gmail.com>
2019-02-20 22:03:37 +0300

    core changes

--- MyFirstCoreApplication/Controllers/ValuesController.cs
+++ MyFirstCoreApplication/Controllers/ValuesController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using CefSharp;
 using Microsoft.AspNetCore.Mvc;
+using CefSharp.OffScreen;
 
 namespace MyFirstCoreApplication.Controllers
 {
@@ -10,6 +14,8 @@ namespace MyFirstCoreApplication.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private static ChromiumWebBrowser browser;
+        
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
@@ -19,15 +25,45 @@ namespace MyFirstCoreApplication.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<string> Get(string id)
         {
-            return "value";
+            string url = "https://artists.spotify.com/c/access/artist/" + id;
+            CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
+
+            var settings = new CefSettings()
+            {
+                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
+            };
+
+            //Perform dependency check to make sure all relevant resources are in our output directory.
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+
+            // Create the offscreen Chromium browser.
+            browser = new ChromiumWebBrowser(url);
+
+            // An event that is fired when the first page is finished loading.
+            // This returns to us from another thread.
+            while (browser.IsLoading)
+            {
+                Thread.Sleep(500);
+            }
+
+            var source = browser.GetSourceAsync().Result;
+            // We have to wait for something, otherwise the process will exit too soon.
+
+            // Clean up Chromium objects.  You need to call this in your application otherwise
+            // you will get a crash when closing.
+            Cef.Shutdown();
+            return source;
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
+
+            
         }
 
         // PUT api/values/5
